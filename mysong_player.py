c5d208bf761768bba86e1fba739486217fb7d903
libaishun <1057437122@qq.com>
2018-02-10 15:59:13 +0800

    production running

--- mysong_player.py
+++ mysong_player.py
@@ -5,12 +5,15 @@ from gpiozero import Button,LED
 from song_player import song_player
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--playlist',default='default',help='default_playlist')
+parser.add_argument('--path',default='default',help='default_path')
+parser.add_argument('--socket_file',default='default',help='default_socket_file')
 args = parser.parse_args()
-playlist = args.playlist
+#playlist = args.playlist
+path = args.path
+socket_file = args.socket_file
 button = Button(2)
-song=song_player(playlist)
-button.when_pressed = song.play
-button.when_released = song.stop
+song=song_player(path,socket_file)
+button.when_pressed = song.music_play
+button.when_released = song.music_pause
 
 pause()
--- song_player.py
+++ song_player.py
@@ -1,9 +1,25 @@
 import os
 class song_player:
-    def __init__(self,playlist):
-        self.playlist = playlist
-    def play(self,start_song=0):
-        play_cmd = 'mpv --no-video --playlist='+self.playlist+' --playlist-start='+str(start_song)+' --save-position-on-quit --loop &'
+    def __init__(self,path,socket_file):
+        self.path = path
+        self.socket_file = socket_file
+        self.start_cmd = 'mpv --no-video '+self.path+' --loop --input-ipc-server='+self.socket_file+' &'
+        self.play_cmd = 'echo \'{ "command": ["set_property", "pause", "no"] }\' |socat - '+self.socket_file
+        self.pause_cmd = 'echo \'{ "command": ["set_property", "pause", "yes"] }\' |socat - '+self.socket_file
+        self.start()
+    def start(self):
+        os.system(self.start_cmd)
+        os.system('sleep 3')
+        os.system(self.pause_cmd)
+    def music_play(self):
+        os.system(self.play_cmd)
+    def music_pause(self):
+        os.system(self.pause_cmd)
+    def playlist(self,playlist,start_song=0):
+        play_cmd = 'mpv --no-video --playlist='+playlist+' --playlist-start='+str(start_song)+' --save-position-on-quit --loop &'
+        os.system(play_cmd)
+    def play_path(self):
+        play_cmd = 'mpv --no-video '+self.path+' --loop &'
         os.system(play_cmd)
     def stop(self):
         stop_cmd='kill -9 `ps aux |grep mpv |grep -v "grep"|awk \'{print $2}\'`'
